1e0fc1a09214bc13e8025a175b11c9cbdf8de85b
Andres Serrano <a.serranoc@uniandes.edu.co>
2023-10-01 18:10:18 -0500

    ReservaController

--- src/main/java/uniandes/repositorio/ReservaRepository.java
+++ src/main/java/uniandes/repositorio/ReservaRepository.java
@@ -11,33 +11,34 @@ import org.springframework.transaction.annotation.Transactional;
 
 import uniandes.edu.co.proyecto.modelo.Reserva;
 
-public interface ReservaRepository extends JpaRepository <Reserva, Integer> {
+public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
-    @Query(value = "SELECT.* FROM reservas",nativeQuery = true)
+    @Query(value = "SELECT.* FROM reservas", nativeQuery = true)
     Collection<Reserva> darReservas();
 
-    @Query(value= "SELECT.* FROM bares WHERE id = :id",nativeQuery = true)
+    @Query(value = "SELECT.* FROM bares WHERE id = :id", nativeQuery = true)
     Reserva darReserva(@Param("id") int id);
 
     @Modifying
     @Transactional
     @Query(value = "INSERT INTO reservas (fechaEntrada,FechaSalida,NumeroPersonas,Cliente_NumerodeIdentificacion,Estado,Empleado_id) VALUES(hotel_sequence.nextval, :fechaEntrada, :FechaSalida, :NumeroPersonas, :Cliente_NumerodeIdentificacion, :Estado, :Empleado_id )")
-    void insertarReserva(@Param("fechaEntrada") Date fechaentrada, @Param("FechaSalida") Date fechasalida, 
-    @Param("NumeroPersonas") Integer numeropersonas,@Param("Cliente_NumerodeIdentificacion") Integer numerodocumento
-    ,@Param("Estado") String estado,@Param("Empleado_id") Integer id_empleado  );
-
+    void insertarReserva(@Param("fechaEntrada") Date fechaentrada, @Param("FechaSalida") Date fechasalida,
+            @Param("NumeroPersonas") Integer numeropersonas,
+            @Param("Cliente_NumerodeIdentificacion") Integer numerodocumento, @Param("Estado") String estado,
+            @Param("Empleado_id") Integer id_empleado);
 
     @Modifying
     @Transactional
     @Query(value = "UPDATE reservas SET fechaEntrada =:fechaEntrada, FechaSalida=:FechaSalida, NumeroPersonas=:NumeroPersonas, Cliente_NumeroIdentificacion=:Cliente_NumerodeIdentificacion, Estado=:Estado, Empleado_id=:Empleado_id WHERE id =:id", nativeQuery = true)
-    void actualizarReserva(@Param("id") int id, @Param("fechaEntrada") Date fechaentrada, @Param("FechaSalida") Date fechasalida, 
-    @Param("NumeroPersonas") Integer numeropersonas,@Param("Cliente_NumerodeIdentificacion") Integer numerodocumento
-    ,@Param("Estado") String estado, @Param("Empleado_id") Integer id_empleado);
+    void actualizarReserva(@Param("id") int id, @Param("fechaEntrada") Date fechaentrada,
+            @Param("FechaSalida") Date fechasalida,
+            @Param("NumeroPersonas") Integer numeropersonas,
+            @Param("Cliente_NumerodeIdentificacion") Integer numerodocumento, @Param("Estado") String estado,
+            @Param("Empleado_id") Integer id_empleado);
 
     @Modifying
     @Transactional
-    @Query(value = "DELETE FROM reservas WHERE id=.id",nativeQuery = true)
+    @Query(value = "DELETE FROM reservas WHERE id=.id", nativeQuery = true)
     void eliminarReserva(@Param("id") int id);
-    
-    
+
 }
