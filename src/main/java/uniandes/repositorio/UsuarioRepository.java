09d45a0c98bffa494ccdb0758375db3b911ed234
Samuel Ramirez <s.ramirezg23@uniandes.edu.co>
2023-10-01 20:57:21 -0500

    repo

--- src/main/java/uniandes/repositorio/UsuarioRepository.java
+++ src/main/java/uniandes/repositorio/UsuarioRepository.java
@@ -13,26 +13,26 @@ import uniandes.edu.co.proyecto.modelo.Usuario;
 public interface UsuarioRepository {
 
      @Query(value = "SELECT.* FROM usuarios", nativeQuery = true)
-    Collection<Usuario> darClientes();
+    Collection<Usuario> darUsuarios();
 
     @Query(value = "SELECT.* FROM usuarios WHERE id = :id", nativeQuery = true)
-    Usuario darCliente(@Param("id") int id);
+    Usuario darUsuario(@Param("id") int id);
 
     @Modifying
     @Transactional
     @Query(value = "INSERT INTO usuarios (Nombre_2,Apellido,Tipo) VALUES(hotel_sequence.nextval, :Nombre_2, :Apellido, :Tipo )")
-    void insertarCliente(@Param("Nombre_2") String Nombre_2, @Param("Apellido") String Apellido,
+    void insertarUsuario(@Param("Nombre_2") String Nombre_2, @Param("Apellido") String Apellido,
             @Param("Tipo") String Tipo);
 
     @Modifying
     @Transactional
     @Query(value = "UPDATE usuarios SET Nombre_2 =:Nombre_2, Apellido=:Apelliod, Tipo=:Tipo WHERE id =:id", nativeQuery = true)
-    void actualizarCliente(@Param("Nombre_2") String Nombre_2, @Param("Apellido") String Apellido,
+    void actualizarUsuario(@Param("Nombre_2") String Nombre_2, @Param("Apellido") String Apellido,
             @Param("Tipo") String Tipo);
 
     @Modifying
     @Transactional
     @Query(value = "DELETE FROM usuarios WHERE id=:id", nativeQuery = true)
-    void eliminarCliente(@Param("id") int id);
+    void eliminarUsuario(@Param("id") int id);
     
 }
