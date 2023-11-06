1413c3dc2a79ec87a3303dcb0c2737cd56bdd0f1
Samuel Ramirez <s.ramirezg23@uniandes.edu.co>
2023-11-06 09:57:51 -0500

    Prueba

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,15 +1,34 @@
 package uniandes.edu.co.proyecto;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import uniandes.edu.co.proyecto.modelo.Cliente;
+import uniandes.edu.co.proyecto.modelo.Servicio;
+import uniandes.edu.co.proyecto.repositorio.ClienteRepository;
+import uniandes.edu.co.proyecto.repositorio.ServicioRepository;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "uniandes.edu.co.protecto.controller")
-public class ProyectoApplication {
+public class ProyectoApplication implements CommandLineRunner {
+	@Autowired private ClienteRepository clienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+	@Override
+	public void run(String ... arg){
+		Collection<Cliente> servicios = clienteRepository.darClientes();
+		for (Cliente s: servicios)
+		{
+			System.out.println(s);
+		}
 	}
 
 }
